11ed0ba232032765a64f80329940d7882753a99c
chanin <pci2676@gmail.com>
2022-09-04 16:45:44 +0900

    good-game-server-1 SecurityFailureHandler 적용
     - `AuthenticationFailureHandler`가 `AuthenticationException`를 handle 하고 있기 때문에 일반 `RuntimeException`은 handle 하고 있지 않았음.

--- goodgame-security/src/main/kotlin/people/songpagu/goodgame/security/domain/member/service/WebSecurityUserService.kt
+++ goodgame-security/src/main/kotlin/people/songpagu/goodgame/security/domain/member/service/WebSecurityUserService.kt
@@ -2,9 +2,12 @@ package people.songpagu.goodgame.security.domain.member.service
 
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+import org.springframework.security.oauth2.core.OAuth2Error
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Component
 import people.songpagu.goodgame.domain.member.model.LoginMember
+import people.songpagu.goodgame.security.domain.member.exception.GoodGameLoginException
 import people.songpagu.goodgame.security.domain.member.service.Oauth2MemberExtractor.Oauth2ValidatedMember
 import javax.servlet.http.HttpSession
 
@@ -20,8 +23,18 @@ class WebSecurityUserService(
         val extractor: Oauth2MemberExtractor = extractorGroup.first { it.enableExtract(userRequest.clientRegistration.registrationId) }
         val oauth2Member: Oauth2ValidatedMember = extractor.extract(oauth2User.attributes)
 
-        val loginMember: LoginMember = securityMemberService.signUpOrIn(oauth2Member)
-        httpSession.setAttribute(LoginMember.KEY, loginMember)
-        return oauth2User
+        return wrapException {
+            val loginMember: LoginMember = securityMemberService.signUpOrIn(oauth2Member)
+            httpSession.setAttribute(LoginMember.KEY, loginMember)
+            oauth2User
+        }
+    }
+
+    private fun wrapException(supplier: () -> OAuth2User): OAuth2User {
+        return try {
+            supplier.invoke()
+        } catch (e: GoodGameLoginException) {
+            throw OAuth2AuthenticationException(OAuth2Error(e.code.value), e.exposureMessage)
+        }
     }
 }

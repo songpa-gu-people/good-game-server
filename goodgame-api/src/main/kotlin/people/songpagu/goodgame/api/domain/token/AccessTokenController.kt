bbfdbab130580681cdb462a7048ea040427b562d
윤성학 <eddy@myfair.co>
2022-10-08 19:38:22 +0900

    refactor: 토큰 발급 실패 에러 추가

--- goodgame-api/src/main/kotlin/people/songpagu/goodgame/api/domain/token/AccessTokenController.kt
+++ goodgame-api/src/main/kotlin/people/songpagu/goodgame/api/domain/token/AccessTokenController.kt
@@ -1,10 +1,11 @@
 package people.songpagu.goodgame.api.domain.token
 
-import org.springframework.web.bind.annotation.CookieValue
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MissingRequestCookieException
+import org.springframework.web.bind.annotation.*
 import people.songpagu.goodgame.api.config.common.response.ApiResponse
 import people.songpagu.goodgame.api.domain.token.handler.AccessTokenGenerateHandler
+import people.songpagu.goodgame.domain.exception.GoodGameCode
 import people.songpagu.goodgame.security.domain.oauth.cookie.GoodGameOauthCookie.Companion.REFRESH_TOKEN_NAME
 
 @RestController
@@ -16,5 +17,13 @@ class AccessTokenController(
     fun getAccessToken(@CookieValue(REFRESH_TOKEN_NAME) refreshToken: String): ApiResponse<String> {
         val accessToken = accessTokenGenerateHandler.issue(refreshToken)
         return ApiResponse.Ok(accessToken)
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingRequestCookieException::class)
+    fun handleException(e: MissingRequestCookieException): ApiResponse<Void> {
+        return ApiResponse.Fail(
+            code = GoodGameCode.ACCESS_TOKEN_FAIL,
+        )
     }
 }
--- goodgame-domain/src/main/kotlin/people/songpagu/goodgame/domain/exception/GoodGameCode.kt
+++ goodgame-domain/src/main/kotlin/people/songpagu/goodgame/domain/exception/GoodGameCode.kt
@@ -3,10 +3,12 @@ package people.songpagu.goodgame.domain.exception
 enum class GoodGameCode(val value: String, val exposureMessage: String) {
     OK("0000", "성공"),
 
-    LOGIN_FAIL("F000", "로그인 실패"),
-
     INTERNAL_SERVER_ERROR("E000", "서버에러"),
 
+    LOGIN_FAIL("F000", "로그인 실패"),
+
     METHOD_ARGUMENT_NOT_VALID("M000",""),
+
+    ACCESS_TOKEN_FAIL("T000", ""), //토큰 발급 실패
     ;
 }
